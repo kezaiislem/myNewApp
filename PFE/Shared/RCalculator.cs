ffe70480f3f0845d653b47793ed764c6c9b08784
Kezai Islem <kezaiislem3@gmail.com>
2020-07-28 23:52:31 +0100

    Adding R calculation functions ( PCA, KMO , Bartlett)

--- PFE/Shared/RCalculator.cs
+++ PFE/Shared/RCalculator.cs
@@ -1,6 +1,7 @@
 ﻿using RDotNet;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -14,7 +15,7 @@ namespace PFE.Shared
 
         public static REngine getInststance()
         {
-            if(instance == null)
+            if (instance == null)
             {
                 REngine.SetEnvironmentVariables();
                 instance = REngine.GetInstance();
@@ -25,7 +26,7 @@ namespace PFE.Shared
 
         public static void Clear()
         {
-            if(instance != null)
+            if (instance != null)
             {
                 instance.ClearGlobalEnvironment();
             }
@@ -49,6 +50,64 @@ namespace PFE.Shared
             //output
             Console.WriteLine("");
             Console.WriteLine("Result: '{0}'", result);
+        }
+
+        public static void BartlettStats(String csvPath)
+        {
+            REngine engine;
+            //init the R engine            
+            engine = getInststance();
+
+            //executing script
+            engine.Evaluate("data <- read.csv('C:/Users/ISLEM/Desktop/Sheet.csv', sep = ';')\n" +
+                "R <- cor(data)\n" +
+                "n <- nrow(data)\n" +
+                "p <- ncol(data)\n" +
+                "chi2 <- -(n-1-(2*p+5)/6)*log(det(R))\n" +
+                "ddl <- p*(p-1)/2\n" +
+                "pch <- pchisq(chi2,ddl,lower.tail=F)");
+
+            double chi2 = Double.Parse(engine.GetSymbol("chi2").AsCharacter()[0], CultureInfo.InvariantCulture);
+            double ddl = Double.Parse(engine.GetSymbol("ddl").AsCharacter()[0], CultureInfo.InvariantCulture);
+            double pch = Double.Parse(engine.GetSymbol("pch").AsCharacter()[0], CultureInfo.InvariantCulture);
+
+            Console.WriteLine("chi2 = " + chi2 + " dll = " + ddl + " pch = " + pch);
+            Clear();
+        }
+
+        public static void KMOStats(String csvPath)
+        {
+            REngine engine;
+            //init the R engine            
+            engine = getInststance();
+
+            //executing script
+            engine.Evaluate("data <- read.csv('C:/Users/ISLEM/Desktop/Sheet.csv', sep = ';')\n" +
+                "R <- cor(data)\n" +
+                "A <- solve(R)\n" +
+                "kmo.num <- sum(R^2) - sum(diag(R^2))\n" +
+                "kmo.denom <- kmo.num + (sum(A^2) - sum(diag(A^2)))\n" +
+                "kmo <- kmo.num/kmo.denom");
+
+            double kmo = Double.Parse(engine.GetSymbol("kmo").AsCharacter()[0], CultureInfo.InvariantCulture);
+
+            Console.WriteLine("KMO = " + kmo);
+            Clear();
+        }
+
+        public static void PCA(String csvPath)
+        {
+            REngine engine;
+            //init the R engine            
+            engine = getInststance();
+
+            //executing script
+            engine.Evaluate("data <- read.csv('C:/Users/ISLEM/Desktop/Sheet.csv', sep = ';')\n" +
+                "pca <- princomp(data, cor=T)\n" +
+                "part <- pca$sdev^2/sum(pca$sdev^2)*100\n" +
+                "biplot(pca)");
+
+            Clear();
         }
     }
 }
